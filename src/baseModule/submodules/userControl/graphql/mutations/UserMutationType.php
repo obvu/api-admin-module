e0b387cb29a068f65688a45bb78848e9009ad147
nik <konialryf@gmail.com>
2019-04-24 14:43:21 +0300

    change password mutation

--- src/baseModule/submodules/userControl/graphql/mutations/UserMutationType.php
+++ src/baseModule/submodules/userControl/graphql/mutations/UserMutationType.php
@@ -11,6 +11,8 @@ namespace Obvu\Modules\Api\Admin\submodules\userControl\graphql\mutations;
 
 use app\models\work\user\object\UserObject;
 use GraphQL\Type\Definition\Type;
+use Yii;
+use yii\web\BadRequestHttpException;
 use Zvinger\BaseClasses\app\graphql\base\BaseGraphQLObjectType;
 
 class UserMutationType extends BaseGraphQLObjectType
@@ -29,6 +31,24 @@ class UserMutationType extends BaseGraphQLObjectType
                         ],
                         'resolve' => function (UserObject $userObject, $args) {
                             $userObject->setAttributes($args);
+                            return $userObject->save();
+                        }
+                    ],
+                    'changePassword' => [
+                        'type' => Type::boolean(),
+                        'args' => [
+                            'old_password' => Type::nonNull(Type::string()),
+                            'new_password' => Type::nonNull(Type::string()),
+                            'confirm_new_password' => Type::nonNull(Type::string()),
+                        ],
+                        'resolve' => function (UserObject $userObject, $args) {
+                            if (!Yii::$app->security->validatePassword($args['old_password'], $userObject->password_hash)) {
+                                throw new BadRequestHttpException('Не верный текущий пароль');
+                            }
+                            if ($args['new_password'] !== $args['confirm_new_password']) {
+                                throw new BadRequestHttpException('Пароли не освпадают');
+                            }
+                            $userObject->password_hash = Yii::$app->security->generatePasswordHash($args['new_password']);
                             return $userObject->save();
                         }
                     ],
